360af40cdfe9224e0dc7db0da6c8a361b00f5576
Sandra Coburn <sandra_coburn@hotmail.com>
2020-08-06 10:05:26 -0700

    Look up table, Big O(1) time complexity

---
+++ sorting challenges/lookup.py
@@ -0,0 +1,13 @@
+import math
+lookup_table = {}
+'''We can buil up the table first before the function or inside the function'''
+# for i in range(1, 1001):
+#     lookup_table[i] = 1/math.sqrt(i)
+def inv_sqrt(n):
+    '''n is an integer between 1 and 1000'''
+    if n not in lookup_table:
+        lookup_table[n] = 1/math.sqrt(n)
+    # return 1 / math.sqrt(n)
+    return lookup_table[n]
+print(inv_sqrt(10))
+print(inv_sqrt(100))
